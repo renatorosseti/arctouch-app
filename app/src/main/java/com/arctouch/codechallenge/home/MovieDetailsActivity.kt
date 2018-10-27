8b091a1f90a21ee641ac7823bef0a1b552bec87c
Renato Rosseti <renato.rosseti@plyom.com.br>
2018-10-27 19:40:27 -0200

    Create Movie details screen and update gradle file with new libraries.

---
+++ app/src/main/java/com/arctouch/codechallenge/home/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.arctouch.codechallenge.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.arctouch.codechallenge.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
